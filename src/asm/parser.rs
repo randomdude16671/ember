2fbd5fd0dfe3a1950a79ac3df56def22dab40fd3
Random Dude <randomdude16671@proton.me>
2025-11-28 19:30:26 +0530

    feat: continue working on parser

--- src/asm/parser.rs
+++ src/asm/parser.rs
@@ -2,23 +2,67 @@
 use crate::asm::errors::*;
 use crate::asm::tokenizer::*;
 
-trait Node {
-    fn string() -> String;
+pub trait Node {
+    fn string(&self) -> String;
 }
 
-trait Expression {
-    fn string() -> String;
-    fn expression_node() -> Self;
+pub trait Expression {
+    fn string(&self) -> String;
+    fn expression_node(&self) -> Self;
 }
 
-trait Statement {
-    fn string() -> String;
-    fn statement_node() -> Self;
+pub trait Statement {
+    fn string(&self) -> String;
+    fn statement_node(&self) -> Self
+    where
+        Self: Sized;
+}
+
+// String Builder
+pub struct Sb {
+    strings: Vec<String>,
+    separator: char,
+    append_count: usize,
+}
+
+impl Sb {
+    pub fn new(sep: char) -> Self {
+        Self {
+            strings: Vec::new(),
+            separator: sep,
+            append_count: 0_usize,
+        }
+    }
+
+    pub fn result(&mut self) -> String {
+        let mut result_string = String::new();
+        for str in &self.strings {
+            result_string.push_str(str.as_str());
+            result_string.push(self.separator);
+        }
+        result_string
+    }
+
+    pub fn write_string(&mut self, string_to_append: String) {
+        self.strings.push(string_to_append);
+        self.append_count += 1;
+    }
+}
+
+pub struct Program {
+    statements: Vec<Box<dyn Statement>>,
+}
+
+impl Program {
+    pub fn string() -> String {
+        let mut out = Sb::new('f');
+        out.result()
+    }
 }
 
 pub struct Parser<'a> {
     t: &'a Tokenizer<'a>,
-    position: Rpos, // struct { ln: usize, col: usize }::new() => struct { .ln = 1, .col = 0 };
+    position: Rpos,
     errors: Vec<EmberParseError>,
 }
 
